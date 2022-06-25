06a905b67743fe3a1941d96c6ae94d225e6c4be2
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-06-25 02:00:06 -0400

    SumOfInput

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        System.out.println("Enter a number");
+        Scanner scan = new Scanner(System.in);
+        int value = scan.nextInt(); //getting the number from the user and storing it in value
+
+        int sum =0;
+        int x = 0;
+        while (x <= value){
+            sum = sum + x;
+            x= x + 1;
+        }
+
+        System.out.println(sum);
+
+    //    for (x =1, x <= value, x++){
+    //        sum += x;
+    //        x++;
+    //    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
